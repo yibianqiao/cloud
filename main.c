def11a61dfab6247563f563c1acc85b822f90a38
yibianqiao <1225977401@qq.com>
2022-05-15 12:53:04 +0800

    测试程序

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int i = 0;
+
+int main(int argc, char *argv[]){
+    // unsigned int i = 0x00ff0011;
+    // unsigned char j = 0;
+    // i = 1;
+    printf("hello world\n");
+
+    return 0;
+}
